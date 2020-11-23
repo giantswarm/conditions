4792dba46e4a75c043426a14af60f62a0fa9e383
Nikola Prokopić <nikola@prokopic.rs>
2020-11-23 16:54:41 +0100

    Add CheckOption to IsNodePoolsReadyFalse

--- pkg/conditions/nodepoolsready.go
+++ pkg/conditions/nodepoolsready.go
@@ -24,9 +24,23 @@ func IsNodePoolsReadyTrue(cluster *capi.Cluster) bool {
 }
 
 // IsNodePoolsReadyFalse checks if specified object is not in NodePoolsReady
-// condition (if NodePoolsReady condition is set with status False).
-func IsNodePoolsReadyFalse(cluster *capi.Cluster) bool {
-	return capiconditions.IsFalse(cluster, NodePoolsReady)
+// condition (if NodePoolsReady condition is set with status False) and if
+// optionally specified checks are successful.
+func IsNodePoolsReadyFalse(cluster *capi.Cluster, checkOptions ...CheckOption) bool {
+	condition := capiconditions.Get(cluster, NodePoolsReady)
+	if !IsFalse(condition) {
+		// Condition is not set or it does not have status False
+		return false
+	}
+
+	for _, checkOption := range checkOptions {
+		if !checkOption(condition) {
+			// additional check has failed
+			return false
+		}
+	}
+
+	return true
 }
 
 // IsNodePoolsReadyUnknown checks if it is unknown whether the specified cluster
@@ -36,12 +50,12 @@ func IsNodePoolsReadyUnknown(cluster *capi.Cluster) bool {
 	return capiconditions.IsUnknown(cluster, NodePoolsReady)
 }
 
-// ReconcileNodePoolsReady sets NodePoolsReady condition on specified cluster
+// UpdateNodePoolsReady sets NodePoolsReady condition on specified cluster
 // by aggregating Ready conditions from specified node pool objects.
 //
 // If node pool objects are found, cluster NodePoolsReady is set to with status
 // False and reason NodePoolsNotFoundReason.
-func ReconcileNodePoolsReady(cluster *capi.Cluster, nodePools []capiconditions.Getter) {
+func UpdateNodePoolsReady(cluster *capi.Cluster, nodePools []capiconditions.Getter) {
 	if len(nodePools) == 0 {
 		capiconditions.MarkFalse(
 			cluster,
