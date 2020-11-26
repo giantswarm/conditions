7197c92a096aaa059a6e82c9fcd47c55e1fe6ce5
Nikola Prokopić <nikola@prokopic.rs>
2020-11-26 10:01:34 +0100

    Move ControlPlaneReady and InfrastructureReady update funcs to conditions-handler repo

--- pkg/conditions/controlplaneready.go
+++ pkg/conditions/controlplaneready.go
@@ -1,7 +1,6 @@
 package conditions
 
 import (
-	"fmt"
 	"time"
 
 	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
@@ -91,58 +90,4 @@ func IsControlPlaneReadyFalse(cluster *capi.Cluster, checkOptions ...CheckOption
 // condition is not set, or it is set with status Unknown).
 func IsControlPlaneReadyUnknown(cluster *capi.Cluster) bool {
 	return capiconditions.IsUnknown(cluster, ControlPlaneReady)
-}
-
-// UpdateControlPlaneReady sets ControlPlaneReady condition on specified
-// cluster by mirroring Ready condition from specified control plane object.
-//
-// If specified control plane object is nil, object ControlPlaneReady will
-// be set with condition False and Reason ControlPlaneObjectNotFoundReason.
-//
-// If specified control plane object's Ready condition is not set, object
-// ControlPlaneReady will be set with condition False and reason
-// WaitingForControlPlane.
-func UpdateControlPlaneReady(cluster *capi.Cluster, controlPlaneObject Object) {
-	if controlPlaneObject == nil {
-		warningMessage :=
-			"Control plane object of type %T is not found for specified %T object %s/%s"
-
-		capiconditions.MarkFalse(
-			cluster,
-			ControlPlaneReady,
-			ControlPlaneObjectNotFoundReason,
-			capi.ConditionSeverityWarning,
-			warningMessage,
-			controlPlaneObject, cluster, cluster.GetNamespace(), cluster.GetName())
-
-		return
-	}
-
-	clusterAge := time.Since(cluster.GetCreationTimestamp().Time)
-	var fallbackSeverity capi.ConditionSeverity
-	fallbackWarningMessage := ""
-	if clusterAge > WaitingForControlPlaneWarningThresholdTime {
-		// Control plane should be reconciled soon after it has been created.
-		// If it's Ready condition is not set within 10 minutes, that means
-		// that something might be wrong, so we set object's ControlPlaneReady
-		// condition with severity Warning.
-		fallbackSeverity = capi.ConditionSeverityWarning
-		fallbackWarningMessage = fmt.Sprintf(" for more than %s", clusterAge)
-	} else {
-		// Otherwise, if it has been less than 10 minutes since object's
-		// creation, probably everything is good, and we just have to wait few
-		// more minutes.
-		// Note: Upstream Cluster API implementation always just sets severity
-		// Info when control plane object's Ready condition is not set.
-		fallbackSeverity = capi.ConditionSeverityInfo
-	}
-
-	fallbackToFalse := capiconditions.WithFallbackValue(
-		false,
-		capi.WaitingForControlPlaneFallbackReason,
-		fallbackSeverity,
-		fmt.Sprintf("Waiting for control plane object of type %T to have Ready condition set%s",
-			controlPlaneObject, fallbackWarningMessage))
-
-	capiconditions.SetMirror(cluster, ControlPlaneReady, controlPlaneObject, fallbackToFalse)
 }
--- pkg/conditions/infrastructureready.go
+++ pkg/conditions/infrastructureready.go
@@ -1,9 +1,6 @@
 package conditions
 
 import (
-	"fmt"
-	"time"
-
 	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
 	capiconditions "sigs.k8s.io/cluster-api/util/conditions"
 )
@@ -39,10 +36,6 @@ const (
 	// provider-specific infrastructure object is not found. When using this
 	// reason, the condition severity should be set to Warning.
 	InfrastructureObjectNotFoundReason = "InfrastructureObjectNotFound"
-
-	// Waiting time during which InfrastructureReady is set to False with
-	// severity Info. After this threshold time, severity Warning is used.
-	WaitingForInfrastructureWarningThresholdTime = 10 * time.Minute
 )
 
 // GetInfrastructureReady tries to get InfrastructureReady condition from the
@@ -90,59 +83,4 @@ func IsInfrastructureReadyFalse(object Object, checkOptions ...CheckOption) bool
 // condition is not set, or it is set with status Unknown).
 func IsInfrastructureReadyUnknown(object Object) bool {
 	return capiconditions.IsUnknown(object, InfrastructureReady)
-}
-
-// UpdateInfrastructureReady sets InfrastructureReady condition on specified
-// object by mirroring Ready condition from specified infrastructure object.
-//
-// If specified infrastructure object is nil, object InfrastructureReady will
-// be set with condition False and Reason InfrastructureObjectNotFoundReason.
-//
-// If specified infrastructure object's Ready condition is not set, object
-// InfrastructureReady will be set with condition False and reason
-// WaitingForInfrastructure.
-func UpdateInfrastructureReady(object Object, infrastructureObject Object) {
-	if infrastructureObject == nil {
-		warningMessage :=
-			"Corresponding provider-specific infrastructure object of " +
-				"type %T is not found for specified %T object %s/%s"
-
-		capiconditions.MarkFalse(
-			object,
-			InfrastructureReady,
-			InfrastructureObjectNotFoundReason,
-			capi.ConditionSeverityWarning,
-			warningMessage,
-			infrastructureObject, object, object.GetNamespace(), object.GetName())
-
-		return
-	}
-
-	objectAge := time.Since(object.GetCreationTimestamp().Time)
-	var fallbackSeverity capi.ConditionSeverity
-	fallbackWarningMessage := ""
-	if objectAge > WaitingForInfrastructureWarningThresholdTime {
-		// Provider-specific infrastructure object should be reconciled soon
-		// after it has been created. If it's Ready condition is not set within
-		// 10 minutes, that means that something might be wrong, so we set
-		// object's InfrastructureReady condition with severity Warning.
-		fallbackSeverity = capi.ConditionSeverityWarning
-		fallbackWarningMessage = fmt.Sprintf(" for more than %s", objectAge)
-	} else {
-		// Otherwise, if it has been less than 10 minutes since object's
-		// creation, probably everything is good, and we just have to wait few
-		// more minutes.
-		// Note: Upstream Cluster API implementation always just sets severity
-		// Info when infrastructure object's Ready condition is not set.
-		fallbackSeverity = capi.ConditionSeverityInfo
-	}
-
-	fallbackToFalse := capiconditions.WithFallbackValue(
-		false,
-		capi.WaitingForInfrastructureFallbackReason,
-		fallbackSeverity,
-		fmt.Sprintf("Waiting for infrastructure object of type %T to have Ready condition set%s",
-			infrastructureObject, fallbackWarningMessage))
-
-	capiconditions.SetMirror(object, InfrastructureReady, infrastructureObject, fallbackToFalse)
 }
