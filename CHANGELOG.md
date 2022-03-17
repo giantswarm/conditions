57ab9a66aebfe6859343c31483af62625271c3d5
Nikola Prokopić <nikola@prokopic.rs>
2022-03-17 22:00:12 +0100

    Update cluster-api to v1.0.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+
+- Update cluster-api to v1.0.5.
+
 ## [0.4.0] - 2021-09-23
 
 ### Changed
--- pkg/conditions/controlplaneready_test.go
+++ pkg/conditions/controlplaneready_test.go
@@ -6,7 +6,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	capi "sigs.k8s.io/cluster-api/api/v1alpha4"
+	capi "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 func TestGetControlPlaneReady(t *testing.T) {
